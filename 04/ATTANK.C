6a7f2b77824e6fef530b39f8deef41eaee9fa5ed
Paul Davies <lagdotcom@gmail.com>
2017-04-27 21:44:49 +0100

    added more PCX/Sprite functions to GAMELIB, added 04/ATTANK

---
+++ 04/ATTANK.C
@@ -0,0 +1,235 @@
+/* I N C L U D E S /////////////////////////////////////////////////////// */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "graph3.h"
+#include "graph4.h"
+
+/* D E F I N E S ///////////////////////////////////////////////////////// */
+
+#define TANK_SPEED      4
+
+/* M A I N /////////////////////////////////////////////////////////////// */
+
+void main(void)
+{
+	long index;                     /* loop index */
+	sprite tank1,                   /* player sprite */
+		   tank2;                   /* enemy sprite */
+	pcx_picture background_pcx,     /* background imagery */
+				objects_pcx;        /* foreground imagery */
+
+	/* direction; also current frame. 0 is north */
+	int tank1_direction = 0,
+		tank2_direction = 0,
+		done = 0;                   /* exit flag */
+
+	float dx, dy, angle;            /* motion vars */
+
+/* S E C T I O N  1 ////////////////////////////////////////////////////// */
+
+	/* Set the video mode to the 320x200, 256-color mode. */
+	Set_Video_Mode(VGA256);
+
+	/* Load in the background. */
+	PCX_Init(&background_pcx);
+	PCX_Load("outpost.pcx", &background_pcx, 1);
+	PCX_Show_Buffer(&background_pcx);
+
+	/* Put up the title. */
+	Blit_String(90, 2, 7, "A T T A N K ! ! !", 1);
+	PCX_Delete(&background_pcx);
+
+	/* Load the PCX file with the tank cells. */
+	/* Load in the player's imagery. */
+	PCX_Init(&objects_pcx);
+	PCX_Load("tanks.pcx", &objects_pcx, 0);
+
+/* S E C T I O N  2 ////////////////////////////////////////////////////// */
+
+	/* Place tank1 (the player) at the bottom of the screen. */
+	Sprite_Init(&tank1, 160, 150, 0, 0, 0, 0, 16, 16);
+
+	/* Place tank2 (the enemy) at the top of the screen. */
+	Sprite_Init(&tank2, 160, 50, 0, 0, 0, 0, 16, 16);
+
+	/* Grab all 16 images from the tank's PCX picture. */
+	for (index = 0; index < 16; index++) {
+		PCX_Grab_Bitmap(&objects_pcx, &tank1, index, index, 0);
+		PCX_Grab_Bitmap(&objects_pcx, &tank2, index, index, 1);
+	}
+
+	/* Kill the PCX memory and buffers now that we're done with them. */
+	PCX_Delete(&objects_pcx);
+
+/* S E C T I O N  3 ////////////////////////////////////////////////////// */
+
+	/* Point the tanks straight up. */
+	tank1.curr_frame = tank1_direction;
+	tank2.curr_frame = tank2_direction;
+
+	/* Scan the background under the tanks on the first iteration. */
+	Behind_Sprite(&tank1);  /* player */
+	Behind_Sprite(&tank2);  /* enemy */
+
+	while (!done) {
+/* S E C T I O N  4 ////////////////////////////////////////////////////// */
+
+		/* Erase both tanks. */
+		Erase_Sprite(&tank1);
+		Erase_Sprite(&tank2);
+
+/* S E C T I O N  5 ////////////////////////////////////////////////////// */
+
+		/* Test whether user wants to translate or rotate the tank. */
+		if (kbhit()) {
+			/* Reset translation factors. */
+			dx = dy = 0;
+
+			/* Test what key was pressed. */
+			switch (getch()) {
+				case '6':
+					/* Rotate to the right. Make sure to wrap around. */
+					if (++tank1_direction > 15) {
+						tank1_direction = 0;
+					}
+					break;
+
+				case '4':
+					/* Rotate to the left. Make sure to wrap around. */
+					if (--tank1_direction < 0) {
+						tank1_direction = 15;
+					}
+					break;
+
+				case '8':
+					/* Move forward. Based on the direction value, compute
+					the translation factors. */
+					angle = (90 + 360 - 22.5 * (float)tank1_direction);
+
+					/* Compute factors based on angle and speed. */
+					dx = TANK_SPEED * cos(M_PI * angle / 180);
+					dy = TANK_SPEED * sin(M_PI * angle / 180);
+					break;
+
+				case '2':
+					/* Move backward. Based on the direction value, compute
+					the translation factors. */
+					angle = (90 + 360 - 22.5 * (float)tank1_direction);
+
+					/* Compute factors based on angle and speed. */
+					dx = -TANK_SPEED * cos(M_PI * angle / 180);
+					dy = -TANK_SPEED * sin(M_PI * angle / 180);
+					break;
+
+				case 'q':
+					/* Quit. */
+					done = 1;
+					break;
+
+				default: break;
+			}
+
+/* S E C T I O N  6 ////////////////////////////////////////////////////// */
+
+			/* Do the translation. */
+			tank1.x += (int)(dx + .5);
+			tank1.y -= (int)(dy + .5);
+
+			/* Test whether the player bumped into edge. If so, push the
+			player's tank back. */
+
+			/* Set the frame based on the new direction. */
+			tank1.curr_frame = tank1_direction;
+		}
+
+/* S E C T I O N  7 ////////////////////////////////////////////////////// */
+
+		/* Now move the enemy tank. */
+
+		/* Test whether it's time to turn. */
+		if (rand() % 10 == 1) {
+			/* Select the direction to turn. */
+			switch (rand() % 2) {
+				case 0:
+					/* Turn right. */
+					if (++tank2_direction > 15) {
+						tank2_direction = 0;
+					}
+					break;
+
+				case 1:
+					/* Turn left. */
+					if (--tank2_direction < 0) {
+						tank2_direction = 15;
+					}
+					break;
+			}
+
+			/* Set the frame based on the new direction. */
+			tank2.curr_frame = tank2_direction;
+		}
+
+/* S E C T I O N  8 ////////////////////////////////////////////////////// */
+
+		/* Compute the angle in radians. */
+		angle = (90 + 360 - 22.5 * (float)tank2_direction);
+
+		/* Compute factors based on angle and speed. */
+		dx = (TANK_SPEED + rand() % 2) * cos(M_PI * angle / 180);
+		dy = (TANK_SPEED + rand() % 2) * sin(M_PI * angle / 180);
+
+		/* Do the translation. */
+		tank2.x += (int)(dx + .5);
+		tank2.y -= (int)(dy + .5);
+
+/* S E C T I O N  9 ////////////////////////////////////////////////////// */
+
+		/* Test whether enemy has hit an edge. If so, wrap to the other
+		side. */
+		if (tank2.x > (320 - tank2.width)) {
+			tank2.x = 0;
+		} else if (tank2.x < 0) {
+			tank2.x = 319 - tank2.width;
+		}
+
+		if (tank2.y > (200 - tank2.height)) {
+			tank2.y = 0;
+		} else if (tank2.y < 0) {
+			tank2.y = 199 - tank2.height;
+		}
+
+/* S E C T I O N  10 ///////////////////////////////////////////////////// */
+
+		/* Scan the background under both tanks. */
+		Behind_Sprite(&tank1);
+		Behind_Sprite(&tank2);
+
+		/* Draw both tanks. */
+		Draw_Sprite(&tank1);
+		Draw_Sprite(&tank2);
+
+		/* Test for collision. */
+		if (Sprite_Collide(&tank1, &tank2)) {
+			/* Do something spectacular! */
+		}
+
+		/* Delay the main loop for a second so the user can see a solid
+		image. */
+		Delay(1);
+	}
+
+	/* Dissolve the screen... in one line, might I add! */
+	for (index = 0; index <= 300000; index++) {
+		Plot_Pixel_Fast(rand() % 320, rand() % 200, 0);
+	}
+
+	/* lag: Clean up! */
+	Sprite_Delete(&tank1);
+	Sprite_Delete(&tank2);
+
+	/* Go back to text mode. */
+	Set_Video_Mode(TEXT_MODE);
+}
+
--- lib/GRAPH4.C
+++ lib/GRAPH4.C
@@ -107,4 +107,206 @@ void PCX_Show_Buffer(pcx_picture_ptr image)
 	asm rep movsw;                  /* do the string operation */
 	asm pop ds;                     /* restore the data segment */
 }
+
+void PCX_Grab_Bitmap(pcx_picture_ptr image, sprite_ptr sprite,
+	int sprite_frame, int grab_x, int grab_y)
+{
+	/* This function grabs a bitmap from the PCX frame buffer. It uses
+	the convention that the 320x200-pixel matrix is subdivided into a
+	smaller matrix of NxN adjacent squares. */
+	int x_off, y_off, x, y;
+	char far *sprite_data;
+
+	/* First, allocate the memory for the sprite in the structure. */
+	sprite->frames[sprite_frame] = (char far*)
+		farmalloc(sprite->width * sprite->height + 1);
+
+	/* Create an alias to the sprite frame for each of access. */
+	sprite_data = sprite->frames[sprite_frame];
+
+	/* Now load the sprite data into the sprite frame array from the
+	PCX picture. */
+	x_off = sprite->width  * grab_x;
+	y_off = sprite->height * grab_y;
+
+	/* lag: I changed all the +1 here because I don't want to put
+	borders around all of my sprites */
+
+	/* Compute the starting y address. */
+	y_off = y_off * (image->header.width + 1);
+
+	for (y = 0; y < sprite->height; y++) {
+		for (x = 0; x < sprite->width; x++) {
+			/* Get the next byte of the current row and place it into
+			the next position in the sprite frame data buffer. */
+			sprite_data[y * sprite->width + x] =
+				image->buffer[y_off + x_off + x];
+		}
+
+		y_off += image->header.width + 1;
+	}
+
+	sprite->num_frames++;
+}
+
+void Sprite_Init(sprite_ptr sprite, int x, int y, int ac, int as,
+	int mc, int ms, int width, int height)
+{
+	/* This function initializes a sprite with the sent data. */
+	int index;
+
+	sprite->x            = x;
+	sprite->y            = y;
+	sprite->x_old        = x;
+	sprite->y_old        = y;
+	sprite->width        = width;
+	sprite->height       = height;
+	sprite->anim_clock   = ac;
+	sprite->anim_speed   = as;
+	sprite->motion_clock = mc;
+	sprite->motion_speed = ms;
+	sprite->curr_frame   = 0;
+	sprite->state        = SPRITE_DEAD;
+	sprite->num_frames   = 0;
+	sprite->background   = (char far *)farmalloc(width * height + 1);
+
+	/* Set all bitmap pointers to null. */
+	for (index = 0; index < MAX_SPRITE_FRAMES; index++) {
+		sprite->frames[index] = NULL;
+	}
+}
+
+void Sprite_Delete(sprite_ptr sprite)
+{
+	/* This function deletes all the memory associated with
+	a sprite. */
+	int index;
+
+	farfree(sprite->background);
+
+	for (index = 0; index < MAX_SPRITE_FRAMES; index++) {
+		farfree(sprite->frames[index]);
+	}
+}
+
+void Draw_Sprite(sprite_ptr sprite)
+{
+	/* This function draws a sprite on the screen, row by row,
+	really quickly. Note the user of shifting to implement
+	multiplication. */
+	char far *work_sprite;
+	int work_offset = 0, offset, x, y;
+	unsigned char data;
+
+	/* Alias a pointer to the sprite for ease of access. */
+	work_sprite = sprite->frames[sprite->curr_frame];
+
+	/* Compute the offset of the sprite in the video buffer. */
+	offset = (sprite->y << 8) + (sprite->y << 6) + sprite->x;
+
+	for (y = 0; y < sprite->height; y++) {
+		for (x = 0; x < sprite->width; x++) {
+			/* Test for a transparent pixel; that is, 0. If the
+			pixel is not transparent, draw it. */
+			data = work_sprite[work_offset + x];
+
+			if (data) {
+				video_buffer[offset + x] = data;
+			}
+		}
+
+		/* Move to the next line in the video buffer and in the
+		sprite bitmap buffer. */
+		offset      += SCREEN_WIDTH;
+		work_offset += sprite->width;
+	}
+}
+
+void Behind_Sprite(sprite_ptr sprite)
+{
+	/* This function scans the background behind a sprite so that,
+	when the sprite is drawn, the background isn't obliterated. */
+	char far *work_back;
+	int work_offset = 0, offset, y;
+
+	/* Alias a pointer to the background for ease of access. */
+	work_back = sprite->background;
+
+	/* Compute the offset of the sprite in the video buffer. */
+	offset = (sprite->y << 8) + (sprite->y << 6) + sprite->x;
+
+	for (y = 0; y < sprite->height; y++) {
+		/* Copy the next row out of the screen buffer into the
+		sprite background buffer. */
+		memcpy((char far *)&work_back[work_offset],
+			   (char far *)&video_buffer[offset],
+			   sprite->width);
+
+		/* Move to the next line in the video buffer and in the
+		sprite bitmap buffer. */
+		offset      += SCREEN_WIDTH;
+		work_offset += sprite->width;
+	}
+}
+
+void Erase_Sprite(sprite_ptr sprite)
+{
+	/* Replace the background that was behind the sprite. */
+
+	/* This function replaces the background that was saved from
+	where a sprite was going to be placed. */
+	char far *work_back;
+	int work_offset = 0, offset, y;
+
+	/* Alias a pointer to the background for ease of access. */
+	work_back = sprite->background;
+
+	/* Compute the offset of the sprite in the video buffer. */
+	offset = (sprite->y << 8) + (sprite->y << 6) + sprite->x;
+
+	for (y = 0; y < sprite->height; y++) {
+		/* Copy the next row out of the screen buffer into the
+		sprite background buffer. */
+		memcpy((char far *)&video_buffer[offset],
+			   (char far *)&work_back[work_offset],
+			   sprite->width);
+
+		/* Move to the next line in the video buff and in the
+		sprite bitmap buffer. */
+		offset      += SCREEN_WIDTH;
+		work_offset += sprite->width;
+	}
+}
+
+int Sprite_Collide(sprite_ptr sprite_1, sprite_ptr sprite_2)
+{
+	/* This function tests the bounding boxes of each sprite to
+	see whether a collision has occurred. IF so, a 1 is returned;
+	otherwise, a 0 is returned. */
+	int dx, dy;
+	int avgw, avgh; /* lag */
+
+	/* Compute the amount of overlap, if any. */
+	dx = abs(sprite_1->x - sprite_2->x);
+	dy = abs(sprite_1->y - sprite_2->y);
+
+	/* lag: compute the average sprite size, so we can compare
+	against that instead of assuming both sprites are equal size */
+	avgw = (sprite_1->width  + sprite_2->width)  >> 1;
+	avgh = (sprite_1->height + sprite_2->height) >> 1;
+
+	/* Test the x and y extents. Note how the width and height are
+	decreased by a percentage of the actual width and height. This
+	is to make the bounding box a little more realistic, as very
+	seldom will an object be rectangular. This helps to ensure
+	that there's a solid collision. */
+
+	/* lag: might be a good idea to make this scaling factor
+	configurable? */
+	if (dx < (avgw - (avgw >> 3)) && dy < (avgh - (avgh >> 3))) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
 
