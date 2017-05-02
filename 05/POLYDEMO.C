8a200b6b559d1538dd28dc7cb8e9c9e2679a2fa7
Paul Davies <lagdotcom@gmail.com>
2017-05-02 22:24:55 +0100

    added 05/POLYDEMO, library code for it

---
+++ 05/POLYDEMO.C
@@ -0,0 +1,162 @@
+/* I N C L U D E S /////////////////////////////////////////////////////// */
+
+#include "graph3.h"
+#include "graph4.h"
+#include "graph5.h"
+
+/* G L O B A L S ///////////////////////////////////////////////////////// */
+
+/* Global clipping region default value. */
+#define POLY_CLIP_MIN_X		(160 - 30)
+#define POLY_CLIP_MIN_Y		(100 - 30)
+
+#define POLY_CLIP_MAX_X		(160 + 30)
+#define POLY_CLIP_MAX_Y		(100 + 30)
+
+/* F U N C T I O N S ///////////////////////////////////////////////////// */
+
+void Draw_Boundary(int color)
+{
+	/* Draws in the clipping boundary, if the user is interested in seeing
+	it. */
+
+	Bline(POLY_CLIP_MIN_X, POLY_CLIP_MIN_Y,
+	      POLY_CLIP_MAX_X, POLY_CLIP_MIN_Y, color);
+	Bline(POLY_CLIP_MAX_X, POLY_CLIP_MIN_Y,
+	      POLY_CLIP_MAX_X, POLY_CLIP_MAX_Y, color);
+	Bline(POLY_CLIP_MAX_X, POLY_CLIP_MAX_Y,
+	      POLY_CLIP_MIN_X, POLY_CLIP_MAX_Y, color);
+	Bline(POLY_CLIP_MIN_X, POLY_CLIP_MAX_Y,
+	      POLY_CLIP_MIN_X, POLY_CLIP_MIN_Y, color);
+}
+
+/* M A I N /////////////////////////////////////////////////////////////// */
+
+void vset(polygon_ptr poly, int index, int x, int y)
+{
+	poly->vertices[index].x = x;
+	poly->vertices[index].y = y;
+}
+
+void main(void)
+{
+	/* Track whether the clipping engine is on. */
+	int clip_on = 1;
+	polygon p1;			/* Working polygon */
+	int done = 0;		/* System exit flag */
+
+	/* Set initial clipping region. */
+	Set_Clipping_Region(POLY_CLIP_MIN_X, POLY_CLIP_MIN_Y,
+	                    POLY_CLIP_MAX_X, POLY_CLIP_MAX_Y);
+
+	/* Set the video mode to the 320x200, 256-color mode. */
+	Set_Video_Mode(VGA256);
+
+	/* Draw some instructions. */
+	Blit_String(0,  0, 7, "Press Q - To quit.", 1);
+	Blit_String(0, 10, 7, "Press T - To toggle clipping engine.", 1);
+	Blit_String(0, 20, 7, "Use Numeric keypad to translate.", 1);
+
+	/* Create look-up tables for polygon engine. */
+	Create_Tables();
+
+	/* Build up a little spaceship polygon. */
+	vset(&p1,  0,   3, -19);
+	vset(&p1,  1,  12,  -1);
+	vset(&p1,  2,  17,   2);
+	vset(&p1,  3,  17,   9);
+	vset(&p1,  4,   8,  14);
+	vset(&p1,  5,   5,   8);
+	vset(&p1,  6,  -5,   8);
+	vset(&p1,  7,  -8,  14);
+	vset(&p1,  8, -17,   9);
+	vset(&p1,  9, -17,   2);
+	vset(&p1, 10, -12,  -1);
+	vset(&p1, 11,  -3, -19);
+	vset(&p1, 12,  -3,  -8);
+	vset(&p1, 13,   3,  -8);
+
+	/* Set the position of the spaceship. */
+	p1.lxo = 160;
+	p1.lyo = 100;
+
+	/* Fill in important fields. */
+	p1.num_vertices = 14;
+	p1.b_color = 1;
+	p1.closed = 1;
+
+	/* Main event loop */
+	while (!done) {
+		/* Erase the polygon. */
+		p1.b_color = 0;
+		if (clip_on) {
+			Draw_Polygon_Clip(&p1);
+		} else {
+			Draw_Polygon(&p1);
+		}
+
+		/* Erase the origin of the polygon. */
+		Plot_Pixel_Fast(p1.lxo, p1.lyo, 0);
+
+		/* What is the user doing? */
+		if (kbhit()) {
+			switch (getch()) {
+				case '8':
+					/* Move the ship up. */
+					p1.lyo--;
+					break;
+
+				case '2':
+					/* Move the ship down. */
+					p1.lyo++;
+					break;
+
+				case '6':
+					/* Move the ship right. */
+					p1.lxo++;
+					break;
+
+				case '4':
+					/* Move the ship left. */
+					p1.lxo--;
+					break;
+
+				case 't':
+					/* Toggle the clipper. */
+					clip_on = 1 - clip_on;
+					break;
+
+				case 'q':
+					/* It's coming now, Khan!!! */
+					done = 1;
+					break;
+			}
+		}
+
+		/* Rotate the polygon 5 degrees. */
+		Rotate_Polygon(&p1, 5);
+
+		/* Draw the polygon. */
+		if (clip_on) {
+			p1.b_color = 1;
+			Draw_Polygon_Clip(&p1);
+			Blit_String(10, 100, 7, "Clipping on. ", 0);
+		} else {
+			p1.b_color = 12;
+			Draw_Polygon(&p1);
+			Blit_String(10, 100, 7, "Clipping off.", 0);
+		}
+
+		/* Draw a point at origin of polygon. */
+		Plot_Pixel_Fast(p1.lxo, p1.lyo, 15);
+
+		/* Let the user see the clipping region. */
+		Draw_Boundary(10);
+
+		/* Just chill here for 1/18.2th of a second. */
+		Delay(1);
+	}
+
+	/* Reset the video mode back to text. */
+	Set_Video_Mode(TEXT_MODE);
+}
--- lib/GRAPH5.C
+++ lib/GRAPH5.C
@@ -1,6 +1,20 @@
 /* I N C L U D E S /////////////////////////////////////////////////////// */
 
+#include <math.h>
 #include "graph3.h"
+#include "graph5.h"
+
+/* G L O B A L S ///////////////////////////////////////////////////////// */
+
+/* Look-up tables for sine and cosine. */
+static float sin_look[361],
+             cos_look[361];
+
+/* Polygon clipping extents */
+static int poly_clip_min_x,
+           poly_clip_min_y,
+           poly_clip_max_x,
+           poly_clip_max_y;
 
 /* F U N C T I O N S ///////////////////////////////////////////////////// */
 
@@ -87,6 +101,403 @@ void Bline(int xo, int yo, int x1, int y1, unsigned char color)
 			vb_start += y_inc;
 		}
 	}
+}
+
+void Draw_Polygon(polygon_ptr poly)
+{
+	/* This function draws a polygon on the screen without clipping. The
+	caller should make sure that vertices are within bounds of the clipping
+	rectangle. The polygon will always be unfilled regardless of the fill
+	flag. */
+	int index, xo, yo;
+
+	/* Extract the local origin. */
+	xo = poly->lxo;
+	yo = poly->lyo;
+
+	/* Draw the polygon. */
+	for (index = 0; index < poly->num_vertices - 1; index++) {
+		Bline(xo + (int)poly->vertices[index].x,
+			yo + (int)poly->vertices[index].y,
+			xo + (int)poly->vertices[index + 1].x,
+			yo + (int)poly->vertices[index + 1].y,
+			poly->b_color);
+	}
+
+	/* Close the polygon? */
+	if (!poly->closed) {
+		return;
+	}
+
+	Bline(xo + (int)poly->vertices[index].x,
+		yo + (int)poly->vertices[index].y,
+		xo + (int)poly->vertices[0].x,
+		yo + (int)poly->vertices[0].y,
+		poly->b_color);
+}
+
+void Translate_Polygon(polygon_ptr poly, int dx, int dy)
+{
+	/* Translate the origin of the polygon. */
+
+	poly->lxo += dx;
+	poly->lyo += dy;
+}
+
+void Scale_Polygon(polygon_ptr poly, float scale)
+{
+	/* Scale each vertex of the polygon. */
+	int index;
+
+	for (index = 0; index < poly->num_vertices; index++) {
+		/* Multiply by the scaling factor. */
+		poly->vertices[index].x *= scale;
+		poly->vertices[index].y *= scale;
+	}
+}
+
+void Create_Tables(void)
+{
+	/* This function creates the sine and cosine look-up tables. */
+	int index;
+
+	/* Create the tables. */
+	for (index = 0; index <= 360; index++) {
+		cos_look[index] = (float)cos((double)(index * M_PI / 180));
+		sin_look[index] = (float)sin((double)(index * M_PI / 180));
+	}
+}
+
+void Rotate_Polygon(polygon_ptr poly, int angle)
+{
+	/* Rotate each point of the polygon around its local origin.
+	Note: angle is an integer and ranges from -360 to +360. */
+	int index;			/* loop index */
+	float si, cs,		/* values of sine and cosine */
+	      rx, ry;		/* rotated points */
+
+	/* Compute the sine and cosine of the angle to be rotated. */
+	if (angle >= 0) {
+		/* Extract the sine and cosine from the look-up table. */
+		si = sin_look[angle];
+		cs = cos_look[angle];
+	} else {
+		/* Angle is negative; convert to positive */
+		si = sin_look[angle + 360];
+		cs = cos_look[angle + 360];
+	}
+
+	/* Using values for sine and cosine, rotate the point. */
+	for (index = 0; index < poly->num_vertices; index++) {
+		/* Compute rotated values using rotation equations. */
+		rx = poly->vertices[index].x * cs -
+		     poly->vertices[index].y * si;
+		ry = poly->vertices[index].y * cs +
+		     poly->vertices[index].x * si;
+
+		/* Store the rotated vertex back in the structure. */
+			poly->vertices[index].x = rx;
+			poly->vertices[index].y = ry;
+	}
+}
+
+int Clip_Line(int *x1, int *y1, int *x2, int *y2)
+{
+	/* This function clips the sent line using the globally defined clipping
+	region. */
+
+	/* Track whether each endpoint is visible or invisible. */
+	int point_1 = 0, point_2 = 0;
+
+	int clip_always = 0;	/* Used for clipping ovveride. */
+	int xi, yi;				/* Point of intersection. */
+	int right_edge = 0,		/* Which edges are the endpoints beyond? */
+		left_edge = 0,
+		top_edge = 0,
+		bottom_edge = 0;
+	int success = 0;		/* Was clipping successful? */
+	float dx, dy;			/* Used to hold slope deltas. */
+
+/* // S E C T I O N  1 /////////////////////////////////////////////////// */
+
+	/* Test whether line is completely visible. */
+	if ((*x1 >= poly_clip_min_x) && (*x1 <= poly_clip_max_x) &&
+		(*y1 >= poly_clip_min_y) && (*y1 <= poly_clip_max_y)) {
+		point_1 = 1;
+	}
+
+	if ((*x2 >= poly_clip_min_x) && (*x2 <= poly_clip_max_x) &&
+		(*y2 >= poly_clip_min_y) && (*y2 <= poly_clip_max_y)) {
+		point_2 = 1;
+	}
+
+/* // S E C T I O N  2 /////////////////////////////////////////////////// */
+
+	/* Test endpoints. */
+	if (point_1 && point_2) {
+		return 1;
+	}
+
+/* // S E C T I O N  3 /////////////////////////////////////////////////// */
+
+	/* Test whether the line is completely invisible. */
+	if (!point_1 && !point_2) {
+		/* Must test to see whether each endpoint is on the same side of one
+		of the bounding planes created by each clipping-region boundary. */
+
+		/* To the left */
+		if (((*x1 < poly_clip_min_x) && (*x2 < poly_clip_min_x)) ||
+		/* To the right */
+			((*x1 > poly_clip_max_x) && (*x2 > poly_clip_max_x)) ||
+		/* Above */
+			((*y1 < poly_clip_min_y) && (*y2 < poly_clip_min_y)) ||
+		/* Below */
+			((*y1 > poly_clip_max_y) && (*y2 > poly_clip_max_y))) {
+			return 0;
+		}
+
+		/* If we got here, we have the special case where the line cuts into
+		and out of the clipping region. */
+		clip_always = 1;
+	}
+
+/* // S E C T I O N  4 /////////////////////////////////////////////////// */
+
+	/* Take care of the case where either endpoint is in the clipping
+	region. */
+	if (point_1 || clip_always) {
+		/* Compute the deltas. */
+		dx = *x2 - *x1;
+		dy = *y2 - *y1;
+
+		/* Compute what boundary line must be clipped against. */
+		if (*x2 > poly_clip_max_x) {
+			/* Flag the right edge. */
+			right_edge = 1;
+
+			/* Compute intersection with the right edge. */
+			if (dx != 0) {
+				yi = (int)(.5 + (dy / dx) * (poly_clip_max_x - *x1) + *y1);
+			} else {
+				yi = -1;	/* Invalidate the intersection. */
+			}
+		} else if (*x2 < poly_clip_min_x) {
+			/* Flag the left edge. */
+			left_edge = 1;
+
+			/* Compute intersection with the left edge. */
+			if (dx != 0) {
+				yi = (int)(.5 + (dy / dx) * (poly_clip_min_x - *x1) + *y1);
+			} else {
+				yi = -1;	/* Invalidate the intersection. */
+			}
+		}
+
+		/* Horizontal intersections */
+		if (*y2 > poly_clip_max_y) {
+			/* Flag the bottom edge. */
+			bottom_edge = 1;
+
+			/* Compute intersection with the bottom edge. */
+			if (dy != 0) {
+				xi = (int)(.5 + (dx / dy) * (poly_clip_max_y - *y1) + *x1);
+			} else {
+				xi = -1;	/* Invalidate the intersection. */
+			}
+		} else if (*y2 < poly_clip_min_y) {
+			/* Flag the top edge. */
+			top_edge = 1;
+
+			/* Compute intersection with the top edge. */
+			if (dy != 0) {
+				xi = (int)(.5 + (dx / dy) * (poly_clip_min_y - *y1) + *x1);
+			} else {
+				xi = -1;	/* Invalidate the intersection. */
+			}
+		}
+	}
+
+/* // S E C T I O N  5 /////////////////////////////////////////////////// */
+
+	/* Now that we know where the line passed through, compute which edge is
+	the proper intersection. */
+	if (right_edge &&
+		(yi >= poly_clip_min_y && yi <= poly_clip_max_y)) {
+		*x2 = poly_clip_max_x;
+		*y2 = yi;
+
+		success = 1;
+	} else if (left_edge &&
+		(yi >= poly_clip_min_y && yi <= poly_clip_max_y)) {
+		*x2 = poly_clip_min_x;
+		*y2 = yi;
+
+		success = 1;
+	}
+
+	if (bottom_edge &&
+		(xi >= poly_clip_min_x && xi <= poly_clip_max_x)) {
+		*x2 = xi;
+		*y2 = poly_clip_max_y;
+
+		success = 1;
+	} else if (top_edge &&
+		(xi >= poly_clip_min_x && xi <= poly_clip_max_x)) {
+		*x2 = xi;
+		*y2 = poly_clip_min_y;
+
+		success = 1;
+	}
+
+/* // S E C T I O N  6 /////////////////////////////////////////////////// */
+
+	/* Reset the edge flags. */
+	right_edge = left_edge = top_edge = bottom_edge = 0;
+
+	/* Test the second endpoint. */
+	if (point_2 || clip_always) {
+		/* Compute the deltas. */
+		dx = *x1 - *x2;
+		dy = *y1 - *y2;
+
+		/* Compute what boundary line must be clipped against. */
+		if (*x1 > poly_clip_max_x) {
+			/* Flag the right edge. */
+			right_edge = 1;
+
+			/* Compute intersection with the right edge. */
+			if (dx != 0) {
+				yi = (int)(.5 + (dy / dx) * (poly_clip_max_x - *x2) + *y2);
+			} else {
+				yi = -1;	/* Invalidate the intersection. */
+			}
+		} else if (*x1 < poly_clip_min_x) {
+			/* Flag the left edge. */
+			left_edge = 1;
+
+			/* Compute intersection with the left edge. */
+			if (dx != 0) {
+				yi = (int)(.5 + (dy / dx) * (poly_clip_min_x - *x2) + *y2);
+			} else {
+				yi = -1;	/* Invalidate the intersection. */
+			}
+		}
+
+		/* Horizontal intersections */
+		if (*y1 > poly_clip_max_y) {
+			/* Flag the bottom edge. */
+			bottom_edge = 1;
+
+			/* Compute intersection with the bottom edge. */
+			if (dy != 0) {
+				xi = (int)(.5 + (dx / dy) * (poly_clip_max_y - *y2) + *x2);
+			} else {
+				xi = -1;	/* Invalidate the intersection. */
+			}
+		} else if (*y1 < poly_clip_min_y) {
+			/* Flag the top edge. */
+			top_edge = 1;
+
+			/* Compute intersection with the top edge. */
+			if (dy != 0) {
+				xi = (int)(.5 + (dx / dy) * (poly_clip_min_y - *y2) + *x2);
+			} else {
+				xi = -1;	/* Invalidate the intersection. */
+			}
+		}
+	}
+
+/* // S E C T I O N  7 /////////////////////////////////////////////////// */
+
+	/* Now that we know where the line passed through, compute which edge is
+	the proper intersection. */
+	if (right_edge &&
+		(yi >= poly_clip_min_y && yi <= poly_clip_max_y)) {
+		*x1 = poly_clip_max_x;
+		*y1 = yi;
+
+		success = 1;
+	} else if (left_edge &&
+		(yi >= poly_clip_min_y && yi <= poly_clip_max_y)) {
+		*x1 = poly_clip_min_x;
+		*y1 = yi;
+
+		success = 1;
+	}
+
+	if (bottom_edge &&
+		(xi >= poly_clip_min_x && xi <= poly_clip_max_x)) {
+		*x1 = xi;
+		*y1 = poly_clip_max_y;
+
+		success = 1;
+	} else if (top_edge &&
+		(xi >= poly_clip_min_x && xi <= poly_clip_max_x)) {
+		*x1 = xi;
+		*y1 = poly_clip_min_y;
+
+		success = 1;
+	}
+
+/* // S E C T I O N  8 /////////////////////////////////////////////////// */
+	return success;
+}
+
+void Draw_Polygon_Clip(polygon_ptr poly)
+{
+	/* This function draws a polygon on the screen with clipping.
+	The polygon will always be unfilled regardless of the fill flag. */
+	int index,
+		xo, yo,		/* local origin */
+		x1, y1,		/* endpoints of the line being processed */
+		x2, y2;
+
+	/* Extract the local origin. */
+	xo = poly->lxo;
+	yo = poly->lyo;
+
+	/* Draw the polygon. */
+	for (index = 0; index < poly->num_vertices - 1; index++) {
+		/* Extract the line. */
+		x1 = (int)poly->vertices[index].x + xo;
+		y1 = (int)poly->vertices[index].y + yo;
+
+		x2 = (int)poly->vertices[index + 1].x + xo;
+		y2 = (int)poly->vertices[index + 1].y + yo;
+
+		/* Clip the line to the viewing screen and draw it, unless the line
+		is totally invisible. */
+		if (Clip_Line(&x1, &y1, &x2, &y2)) {
+			Bline(x1, y1, x2, y2, poly->b_color);
+		}
+	}
+
+	/* Close the polygon? */
+	if (!poly->closed) {
+		return;
+	}
+
+	/* Extract the line. */
+	x1 = (int)poly->vertices[index].x + xo;
+	y1 = (int)poly->vertices[index].y + yo;
+
+	x2 = (int)poly->vertices[0].x + xo;
+	y2 = (int)poly->vertices[0].y + yo;
+
+	/* Clip the line to the viewing screen and draw it, unless the line
+	is totally invisible. */
+	if (Clip_Line(&x1, &y1, &x2, &y2)) {
+		Bline(x1, y1, x2, y2, poly->b_color);
+	}
+}
+
+void Set_Clipping_Region(int minx, int miny, int maxx, int maxy)
+{
+	/* lag: utility function */
+	poly_clip_min_x = minx;
+	poly_clip_min_y = miny;
+	poly_clip_max_x = maxx;
+	poly_clip_max_y = maxy;
 }
 
 
