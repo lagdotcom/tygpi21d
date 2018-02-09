6bee4f3a21febf37f26f1d3738e12b1c7cf914b6
Paul Davies <lagdotcom@gmail.com>
2018-02-09 08:50:43 +0000

    DOSJUN: added stub files for other classes, renamed all class source to J<something>

---
+++ DOSJUN/JFIGHTER.C
@@ -0,0 +1,48 @@
+/* I N C L U D E S /////////////////////////////////////////////////////// */
+
+#include "dosjun.h"
+
+/* D E F I N E S ///////////////////////////////////////////////////////// */
+
+#define CONCENTRATE_HITBONUS	4
+#define CONCENTRATE_DMGBONUS	4
+
+/* F U N C T I O N S ///////////////////////////////////////////////////// */
+
+bool Check_Concentrate(combatant *source)
+{
+	if (!Has_Skill(source, skConcentrate))
+		return false;
+
+	return Check_Attack(source);
+}
+
+void Concentrate(combatant *source, combatant *target)
+{
+	item *weapon = Get_Weapon(source);
+	stat_value base = Get_Stat(source, Get_Weapon_Stat(weapon));
+	stat_value min, max;
+	int roll;
+
+	if (Is_Dead(target)) {
+		Combat_Message("%s loses their concentration.", source->name);
+		return;
+	}
+
+	base += Get_Stat(source, sHitBonus) + CONCENTRATE_HITBONUS;
+	base -= Get_Stat(target, sDodgeBonus);
+
+	if (randint(1, 20) <= base) {
+		Combat_Message("%s strikes %s true.", source->name, target->name);
+
+		Get_Weapon_Damage(source, weapon, &min, &max);
+		roll = randint(min, max) - Get_Stat(target, sArmour) + CONCENTRATE_DMGBONUS;
+		if (roll > 0) {
+			Damage(target, roll);
+		} else {
+			Combat_Message("The blow glances off.");
+		}
+	} else {
+		Combat_Message("%s narrowly misses %s.", source->name, target->name);
+	}
+}
